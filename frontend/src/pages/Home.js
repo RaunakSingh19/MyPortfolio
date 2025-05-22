5c5cde7579f9dc1cd2129e4342de7061d6492cd4
“raunaksingh19” <raunaksingh142004@gmail.com>
2025-05-21 22:03:02 -0700

    final commit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,8 @@ import { Box } from '@mui/material';
 import HomePage2 from './Homepage2';
 import HomePage from './Homepage';
 // import Navbar from './Navbar';
-import Footer from '../components/Footer';
+import Carousel from '../components/Carousel'
+// import Footer from '../components/Footer';
 
 
 // ✅ Then your component code
@@ -13,6 +14,7 @@ const Home = () => {
       {/* <Navbar /> */}
       <HomePage2 />
       <HomePage />
+      <Carousel />
       {/* <Footer /> */}
     </Box>
   );
